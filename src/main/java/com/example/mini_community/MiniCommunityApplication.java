3f4e01de6e91a3f0255d30b69408ea2d6fa508d4
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-04-01 16:02:12 +0900

    initial commit

---
+++ src/main/java/com/example/mini_community/MiniCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_community/MiniCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
