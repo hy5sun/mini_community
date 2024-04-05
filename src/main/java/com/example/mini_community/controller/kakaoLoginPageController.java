065a0ee84594446da071fe5534c11ddae5fe822c
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-04-05 23:58:00 +0900

    feat: 카카오 로그인 페이지

---
+++ src/main/java/com/example/mini_community/controller/kakaoLoginPageController.java
@@ -0,0 +1,25 @@
+package com.example.mini_community.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class kakaoLoginPageController {
+    @Value("${kakao.client_id}")
+    private String clientId;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirectUri;
+
+    @GetMapping()
+    public String loginPage(Model model) {
+        String reqUrl = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+clientId+"&redirect_uri="+redirectUri;
+        model.addAttribute("reqUrl", reqUrl);
+
+        return "kakaoLogin";
+    }
+}
