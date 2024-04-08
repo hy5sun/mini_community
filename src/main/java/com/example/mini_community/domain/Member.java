52247ea6bf288417e479aea9f3cd490528169fbe
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-04-08 19:49:31 +0900

    feat: member Entity 생성

---
+++ src/main/java/com/example/mini_community/domain/Member.java
@@ -0,0 +1,45 @@
+package com.example.mini_community.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Table(name="member")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String profile_img;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Member(String nickname, String profile_img) {
+        this.nickname = nickname;
+        this.profile_img = profile_img;
+    }
+}
