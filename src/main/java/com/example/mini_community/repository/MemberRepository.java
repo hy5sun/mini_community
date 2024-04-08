0f30f03f574b187a26194b0d0d749bd0e5ee6868
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-04-08 19:53:14 +0900

    feat: MemberRepository 추가

---
+++ src/main/java/com/example/mini_community/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.mini_community.repository;
+
+import com.example.mini_community.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByNickname(String nickname);
+}
