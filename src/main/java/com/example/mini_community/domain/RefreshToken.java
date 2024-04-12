cca310eca803f5d36990083f8491d68c686cdf44
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-04-12 17:32:03 +0900

    feat: RefreshToken 도메인 생성

---
+++ src/main/java/com/example/mini_community/domain/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.example.mini_community.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    public RefreshToken(Long memberId, String refreshToken) {
+        this.memberId = memberId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newToken) {
+        this.refreshToken = newToken;
+        return this;
+    }
+}
---
+++ src/main/java/com/example/mini_community/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.mini_community.repository;
+
+import com.example.mini_community.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByMemberId(Long memberId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
