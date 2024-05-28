2cb0144a3023702cdb14de79d54449f8b9985fc3
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-05-28 16:13:47 +0900

    feat: 예외 처리 적용

--- src/main/java/com/example/mini_community/service/member/MemberService.java
+++ src/main/java/com/example/mini_community/service/member/MemberService.java
@@ -1,11 +1,14 @@
 package com.example.mini_community.service.member;
 
+import com.example.mini_community.common.exception.BusinessException;
 import com.example.mini_community.domain.member.Member;
 import com.example.mini_community.repository.member.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.example.mini_community.common.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +20,12 @@ public class MemberService {
     @Transactional
     public Member findByEmail(String email) {
         return memberRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("해당 이메일에 해당하는 회원이 존재하지 않습니다."));
+                .orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
     }
 
     @Transactional
     public Member findById(Long id) {
         return memberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 id에 해당하는 회원이 존재하지 않습니다."));
+                .orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
     }
 }
