71db6251b5dcf5a6bb1b6bae67f2cfc90146f25d
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-05-30 21:43:27 +0900

    feat: Board Req, Res 생성

---
+++ src/main/java/com/example/mini_community/dto/board/BoardResponse.java
@@ -0,0 +1,24 @@
+package com.example.mini_community.dto.board;
+
+import com.example.mini_community.domain.board.Board;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class BoardResponse {
+    private UUID id;
+    private String title;
+    private String content;
+    private String image;
+    private String writer_nickname;
+    private Integer view_count;
+    private Integer like_count;
+
+    public static BoardResponse entityToDto(Board board) {
+        return new BoardResponse(board.getId(), board.getTitle(), board.getContent(), board.getImage(),
+                board.getMember().getNickname(), board.getView_count(), board.getLike_count());
+    }
+}
