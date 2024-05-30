cf8fb4d4ccf03d757409501a697e1a945e17d8a7
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-05-31 00:37:02 +0900

    feat: Board 전체 조회 API

--- src/main/java/com/example/mini_community/controller/board/BoardController.java
+++ src/main/java/com/example/mini_community/controller/board/BoardController.java
@@ -3,16 +3,16 @@ package com.example.mini_community.controller.board;
 import com.example.mini_community.common.config.annotation.Login;
 import com.example.mini_community.common.response.CustomResponse;
 import com.example.mini_community.domain.member.Member;
+import com.example.mini_community.dto.board.AllBoardsResponse;
 import com.example.mini_community.dto.board.BoardResponse;
 import com.example.mini_community.dto.board.CreateBoardRequest;
 import com.example.mini_community.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/boards")
@@ -26,6 +26,13 @@ public class BoardController {
         BoardResponse board = boardService.createBoard(member, req);
 
         return CustomResponse.response(HttpStatus.CREATED, "게시물을 정상적으로 작성했습니다.", board);
+    }
+
+    @GetMapping()
+    public CustomResponse findAll(@Login Member member) {
+        List<AllBoardsResponse> boards = boardService.findAll();
+
+        return CustomResponse.response(HttpStatus.OK, "모든 게시물을 정상적으로 조회했습니다.", boards);
     }
 
 }
--- src/main/java/com/example/mini_community/service/board/BoardService.java
+++ src/main/java/com/example/mini_community/service/board/BoardService.java
@@ -2,13 +2,17 @@ package com.example.mini_community.service.board;
 
 import com.example.mini_community.domain.board.Board;
 import com.example.mini_community.domain.member.Member;
+import com.example.mini_community.dto.board.AllBoardsResponse;
 import com.example.mini_community.dto.board.BoardResponse;
 import com.example.mini_community.dto.board.CreateBoardRequest;
 import com.example.mini_community.repository.board.BoardRepository;
+import com.example.mini_community.service.member.MemberService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +33,17 @@ public class BoardService {
         boardRepository.save(board);
 
         return BoardResponse.entityToDto(board);
+    }
+
+    @Transactional
+    public List<AllBoardsResponse> findAll() {
+        // TODO : 추후에 category 별로 조회하도록 할 예정
+
+        List<AllBoardsResponse> boards = boardRepository.findAll()
+                .stream()
+                .map((Board board) -> new AllBoardsResponse(board.getTitle(), board.getContent(), board.getImage()))
+                .toList();
+
+        return boards;
     }
 }
